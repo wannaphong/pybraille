5fff02b309bc3705a24890a98b04e7769250df6a
Wannaphong <wannaphong@yahoo.com>
2017-03-29 21:26:54 +0700

    แก้ไข เพิ่ม tobraille() ให้ thai ต้นแบบ

--- thaibraille.py
+++ thaibraille.py
@@ -8,9 +8,16 @@
 - สามารถแปลงข้อความปกติเป็นอักษรเบรลล์ได้
 - สามารถกลับด้าน เพื่อใช้ในการพิมพ์และกดจุดอักษรเบรลล์ได้ เช่น อักษรเบรลล์ เป็น ล์ลรบเรษก ั อ
 '''
-from braille import tob
+from pybraille import braille
 class thaibraille():
 	def word(self,data):
 		self.word=data # รับข้อความเข้ามาในระบบ
+		self.braille = braille()
+		self.data =[]
 	def tobraille(self):
 		self.dict1={"ก":['1234'],"ข":['15'],"ฃ":['456','15'],"ค":['156'],"ฅ":['56','156'],"ฆ":['6','156'],"ง":['12346'],'จ':['234'],'ฉ':['25'],'ช':['256'],'ซ':['2356'],'ฌ':['6','256']}
+		for a in list(self.word):
+			self.braille.inputnum(self.dict1[a])
+			self.data.append(self.braille.tobraille())
+			self.data.append(" ")
+		return ''.join(self.data)
